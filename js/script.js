4b348a6a902a7adca554e63cf79ca66f60c3de19
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-04-10 18:38:24 +0000

    everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Clara Tyman
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+'use strict'
+
+/**
+ * This function converts fahrenheit to celsius
+ */
+function calculate() {
+  // input
+  const fahrenheit = parseFloat(document.getElementById('fahrenheit').value)
+
+  // process
+  const celsius = (fahrenheit - 32) * 5 / 9
+
+  // output
+  document.getElementById('conversion').innerHTML = fahrenheit + '°F converted to celsius is ' + celsius.toFixed(2) + ' °C'
+}
